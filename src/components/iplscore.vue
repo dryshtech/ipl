86a2485d2d1ae81a1a52b613aeb072fe52c62c5c
dryshtech <dryshtech@gmail.com>
2020-03-16 16:56:19 -0700

    change 3/16/20

--- src/components/iplscore.vue
+++ src/components/iplscore.vue
@@ -1,23 +1,17 @@
 <template>
    <div> 
-       <md-card md-with-hover>
-           <md-ripple>
-            <div v-for="result in allResults.events" :key="result.events" class="column">
-            <md-card-header>
-             <div class="md-elevation-22" style="background:lightgreen" >{{ result.description }}</div>
-             </md-card-header>
-             <md-card-content>{{ result.name }}</md-card-content>
-             <md-card-content>{{ 'Result' }} : {{ result.status.summary }}</md-card-content>
+      <div v-for="result in allResults.events" :key="result.events" class="column">
+        <div class="md-elevation-10">{{ result.description }}</div>
+          <md-card-content class="md-elevation-10">{{ result.name }}</md-card-content>
+             <md-card-content class="md-elevation-10">{{ 'Result' }} : {{ result.status.summary }}</md-card-content>
              <div v-for="comp in result.competitions" :key="comp.competitions" class="column">
-                <md-card-content> {{ 'Played At' }} : {{ comp.venue.fullName }}</md-card-content>
+                <md-card-content class="md-elevation-10"> {{ 'Played At' }} : {{ comp.venue.fullName }}</md-card-content>
                 <div v-for="fa in comp.status.featuredAthletes" :key="fa.featuredAthletes" class="column">
-                <md-card-content> {{ 'Player of The Match' }} : {{ fa.athlete.fullName }}</md-card-content>
-                </div>
+                <md-card-content class="md-elevation-10"> {{ 'Player of The Match' }} : {{ fa.athlete.fullName }}</md-card-content>
+               </div>
              </div>    
-            </div>
-            </md-ripple>
-        </md-card>
-   </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -36,16 +30,5 @@
 </script>
 
 <style >
-.results {
-  display: grid;
-  grid-template-columns: repeat(1, 3fr);
-  grid-gap: 5rem;
-  border: 5px solid #ccc;
-  background: #41b883;
-  padding: 3rem;
-  border-radius: 10px;
-  text-align: center;
-  position: relative;
-  cursor: pointer;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdButton, MdRipple, MdCard } from 'vue-material/dist/components'
-import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/black-green-light.css'
-
-Vue.use(MdButton)
-Vue.use(MdRipple)
-Vue.use(MdCard)
 
 import store from './store';
 
